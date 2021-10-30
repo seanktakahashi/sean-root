d5674b054818a8b33ab696b9fdba5fd810a582f5
seanktakahashi <seanktakahashi@gmail.com>
2021-10-30 13:35:05 -0700

    git in prompt

--- zsh/general.sh
+++ zsh/general.sh
@@ -3,7 +3,18 @@
 #   print green check on return code 0, else print red X and return code
 #   print path, limited to depth 3
 #   yellow blinking greater than
-PROMPT="%(?.%F{green}√.%F{red}X%?)%f %F{cyan}%3~%f%B %F{yellow}%(!.#.>)%f%b"
+setopt PROMPT_SUBST
+
+parse_git_branch() {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/-\1-/'
+}
+
+RPROMPT="%K{green}\$(parse_git_branch)%k"
+# Parts
+EXIT_CODE='%(?.%F{green}√.%F{red}X)%f'
+CURR_DIR='%F{cyan}%3~%f%B'
+POINTER='%F{yellow}%(!.#.>)%f%b'
+PROMPT="${EXIT_CODE}${CURR_DIR} ${POINTER}"
 
 #### SEAN BIN ####
 PATH=$SEAN/bin:$PATH
