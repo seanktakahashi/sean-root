7c628ba6ea26a56da8da3b7b1e1a319414314230
blue <blue.laptop.undersofa@gmail.com>
2021-11-06 00:19:37 -0700

    Enable Git Completion for Bash
    - including completion for git aliases

--- zsh/git-aliases.sh
+++ zsh/git-aliases.sh
@@ -18,3 +18,27 @@ gfetch() {
 }
 alias gfix="git commit -am 'fix'"
 alias gsmall="git commit -am 'small'"
+
+bash_git_complete() {
+  git_completed=0
+  FILE="/usr/share/bash-completion/completions/git"
+  if [ -f $FILE ]; then
+    source $FILE
+    git_completed=1
+  fi
+  FILE="/etc/bash_completion.d/git"
+  if [ -f $FILE ]; then
+    source $FILE
+    git_completed=1
+  fi
+  if [ $git_completed -eq 0 ]; then
+    echo "git completion not installed"
+  else
+    __git_complete gc _git_checkout
+    __git_complete gb _git_branch
+    __git_complete gswitch _git_switch
+  fi
+}
+
+bash_git_complete
+
